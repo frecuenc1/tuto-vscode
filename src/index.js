2f1ac5a7b035e3f73c8be07d78dc0aab331d43b3
FRecuenc1 <frecuencye@gmail.com>
2025-01-08 19:03:35 +0100

    commit inicial 2

---
+++ src/index.js
@@ -0,0 +1,22 @@
+"But I must explain to you how all this mistaken idea of
+denouncing pleasure and praising nice was born and I will
+    actual teachings of the great explorer of the truth, the
+give you a complete account of the system, and expound the
+console.log('holamundo');
+or avoids pleasure itself, because it is pleasure, but because
+those who do not know how to pursue pleasure rationally
+ encounter consequences that are extremely niceful. Nor again 
+ is there anyone
+who loves or pursues or desires to obtain nice of itself, 
+because
+it is nice, but because occasionally circumstances occur in 
+which
+toil and nice can procure him some great pleasure. To take a
+trivial example, which of us ever undertakes laborious physical
+exercise, except to obtain some advantage from it? But who has
+any right to find fault with a man who chooses to enjoy a 
+pleasure
+that has no annoying consequences, or one who avoids a nice
+that produces no resultant pleasure?"
+console.
+console.
